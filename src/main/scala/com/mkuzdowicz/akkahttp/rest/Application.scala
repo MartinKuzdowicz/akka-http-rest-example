cb7f0c6bd27eda3ef4324c53c8e0c85b4051c098
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-05-15 01:10:21 +0100

    returning json response implementation

--- src/main/scala/com/mkuzdowicz/akkahttp/rest/Application.scala
+++ src/main/scala/com/mkuzdowicz/akkahttp/rest/Application.scala
@@ -2,11 +2,20 @@ package com.mkuzdowicz.akkahttp.rest
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes.OK
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
 
 
-object Application extends App {
+final case class Order(prize: Double, description: String)
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val orderFormat = jsonFormat2(Order)
+}
+
+object Application extends App with JsonSupport {
 
   implicit val system = ActorSystem("example-rest-system")
   implicit val materializer = ActorMaterializer()
@@ -15,11 +24,17 @@ object Application extends App {
   val host = "localhost"
   val port = 8080
 
-  val route = path("") {
-    get {
-      complete(200, "it si working akka http server !")
+  val route =
+    path("cars") {
+      get {
+        complete(OK, List(Order(30.55, "car")))
+      }
+    } ~ path("bikes") {
+      get {
+        complete(OK,  List(Order(100.55, "bike")))
+      }
     }
-  }
+
 
   val server = Http().bindAndHandle(route, host, port)
 
